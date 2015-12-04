f73fc88e6b1e124bf47328339b520efa4f96a894
Alexandre Maurel <alexandre.maurel@gmail.com>
2015-12-04 10:41:14 +0100

    init

---
+++ R/PamCloudInstall.R
@@ -0,0 +1,74 @@
+
+PamCloud.getBinaryURL= function(url = "https://pamcloud.pamgene.com/jackrabbit/repository/default/PamCloud/BioNavigator/Resources/readme.txt", credentials = login.dialog()){
+  checkpack("RCurl")
+  if(!is.null(credentials)){
+    aFile = getBinaryURL(url, userpwd = paste(credentials$user,credentials$pw, sep = ":"), ssl.verifypeer = FALSE)
+  } else{
+    aFile = getBinaryURL(url, ssl.verifypeer = FALSE)
+  }
+return(aFile)
+}
+
+PamCloud.checkAccess = function(credentials = login.dialog()){
+  aFile = PamCloud.getBinaryURL(credentials = credentials)
+  return(substring(rawToChar(aFile),1,33) == "<PamCloud Bionavigator Resources>")
+}
+
+PamCloud.installPackage = function(packageUrl, tmpDir = "C:\\temp"){
+  credentials = login.dialog()
+  if(!PamCloud.checkAccess(credentials)){
+      stop("PamCloud access denied or not available")
+  }
+  
+  nPack = length(packageUrl)
+  print(paste("Installing", nPack, "packages"))
+  for (i in 1:nPack){
+    aFile = PamCloud.getBinaryURL(packageUrl[i], credentials)
+  
+    if (!file.exists("C:\\temp")){
+      tmpDir = getwd();
+    }
+    pckFile = file.path(tmpDir, basename(packageUrl[i]))     
+    writeBin(aFile, pckFile)
+    install.packages(pckFile, repos = NULL)
+  }
+}
+
+login.dialog = function(){
+  checkpack("tcltk")
+  credentials = list(user = "PamCloud User Name", pw = "pass") 
+  onok = function(){tkcget
+                    tkdestroy(tt)
+  }
+  tt = tktoplevel()
+  label.user = tklabel(tt, text = "User Name")
+  userVal = tclVar(init = credentials$user)
+  entry.user =  tkentry(tt, textvariable = userVal)
+  label.pw = tklabel(tt, text = "Password")
+  pwVal = tclVar(init = credentials$pw)
+  entry.pw = tkentry(tt, textvariable = pwVal, show = "*")
+  button.ok = tkbutton(tt, text = "OK", command =onok)
+  tkpack(label.user)
+  tkpack(entry.user)
+  tkpack(label.pw)
+  tkpack(entry.pw)
+  tkpack(button.ok)
+  tkfocus(tt)
+  tkwait.window(tt)
+  credentials$user = tclvalue(userVal)
+  credentials$pw = tclvalue(pwVal)
+  return(credentials)
+}
+
+checkpack = function(libstr, repos = "http://cran.rstudio.com"){
+  # libstr can be an array of strings with multiple packages
+  for(i in 1:length(libstr)){
+    if (!(libstr[i] %in% installed.packages())){
+      install.packages(libstr[i], dependencies = TRUE, repos = repos)
+    }
+    library(libstr[i], character.only = TRUE)
+  }
+}
+
+
+
